6e2ba50916c66d586c7155bff86c32e702d52907
Zechariah Gaiya <abbazechariah@gmail.com>
2023-11-28 10:43:00 -0800

    Adding 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        if (i !=8 and j != 9):
-            print(f"{i:d}{j:d}".format(i, j), end='')
+for i in range(0, 90):
+    if i % 10 > i / 10:
+        if i != 89:
+            print(f"{i:02d}, ".format(i), end='')
         else:
-            print(f"{i:d}{j:d}".format(i, j))
+            print(f"{i:02d}".format(i))
